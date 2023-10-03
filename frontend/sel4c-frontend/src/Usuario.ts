7706cd41e9230bdff1c66506c635080d0f9fdba1
ArturoCarballo <a01662245@tec.mx>
2023-10-02 20:44:27 -0600

    Inicio prueba

---
+++ frontend/sel4c-frontend/src/Usuario.ts
@@ -0,0 +1,13 @@
+export interface Usuario {
+    id: number;
+    apellido: string;
+    disciplina: string;
+    email: string;
+    edad: number;
+    sexo: string;
+    grado_academico: string;
+    institucion: string;
+    nombre: string;
+    pais: string;
+  }
+  
