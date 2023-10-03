27d650e57956da7be859f2eba4e881d122813b7b
ArturoCarballo <a01662245@tec.mx>
2023-10-02 23:42:44 -0600

    Añadido filtro en tabla

--- frontend/sel4c-frontend/src/components/UserTable.tsx
+++ frontend/sel4c-frontend/src/components/UserTable.tsx
@@ -3,6 +3,8 @@ import { User } from '../interface/User';
 
 export const UserTable: React.FC = () => {
     const [users, setUsers] = useState<User[]>([]);
+    const [sortedUsers, setSortedUsers] = useState<User[]>([]);
+    const [sortConfig, setSortConfig] = useState<{key: keyof User, direction: string} | null>(null);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -17,37 +19,57 @@ export const UserTable: React.FC = () => {
         fetchUsers();
     }, []);
 
+    useEffect(() => {
+        let sortedArray = [...users];
+        if (sortConfig !== null) {
+            const { key, direction } = sortConfig; // Extraemos key y direction aquí
+            sortedArray.sort((a: User, b: User) => {
+                const aValue = a[key];
+                const bValue = b[key];
+            
+                if (aValue != null && bValue != null) {
+                    if (aValue < bValue) {
+                        return direction === 'ascending' ? -1 : 1;
+                    }
+                    if (aValue > bValue) {
+                        return direction === 'ascending' ? 1 : -1;
+                    }
+                }
+                return 0;
+            });
+        }
+        setSortedUsers(sortedArray);
+    }, [users, sortConfig]);
+
+    const requestSort = (key: keyof User) => {
+        let direction = 'ascending';
+        if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+    };
+
     return (
         <div>
             <h2>Usuarios</h2>
             <table>
                 <thead>
                     <tr>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Email</th>
-                        <th>Edad</th>
-                        <th>Sexo</th>
-                        <th>Grado Académico</th>
-                        <th>Institución</th>
-                        <th>País</th>
-                        <th>Disciplina</th>
-                        {/* Agregar más columnas según sea necesario */}
+                        <th onClick={() => requestSort('nombre')}>Nombre</th>
+                        <th onClick={() => requestSort('apellido')}>Apellido</th>
+                        <th onClick={() => requestSort('email')}>Email</th>
+                        <th onClick={() => requestSort('edad')}>Edad</th>
+                        {/* ... más encabezados de columna ... */}
                     </tr>
                 </thead>
                 <tbody>
-                    {users.map((user) => (
+                    {sortedUsers.map((user) => (
                         <tr key={user.id}>
                             <td>{user.nombre}</td>
                             <td>{user.apellido}</td>
                             <td>{user.email}</td>
                             <td>{user.edad}</td>
-                            <td>{user.sexo}</td>
-                            <td>{user.grado_academico}</td>
-                            <td>{user.institucion}</td>
-                            <td>{user.pais}</td>
-                            <td>{user.disciplina}</td>
-                            {/* Agregar más celdas según sea necesario */}
+                            {/* ... más celdas ... */}
                         </tr>
                     ))}
                 </tbody>
