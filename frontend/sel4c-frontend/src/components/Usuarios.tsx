de4f5dbe055c2dd0e83a7600e4baa500e5f683a1
ArturoCarballo <a01662245@tec.mx>
2023-10-02 21:11:20 -0600

    Creacion de la vista Usuarios

---
+++ frontend/sel4c-frontend/src/components/Usuarios.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { Usuario } from '../Usuario';
+
+const Usuarios: React.FC = () => {
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+    useEffect(() => {
+        fetchUsuarios();
+    }, []);
+
+    const fetchUsuarios = async () => {
+        try {
+            const response = await fetch('/api/usuarios');
+            const data = await response.json();
+            setUsuarios(data);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
+    };
+
+    const agregarUsuario = async (usuario: Usuario) => {
+        try {
+            await fetch('/api/usuarios', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(usuario),
+            });
+            fetchUsuarios();
+        } catch (error) {
+            console.error('Error adding user:', error);
+        }
+    };
+
+    const actualizarUsuario = async (id: number, usuario: Usuario) => {
+        try {
+            await fetch(`/api/usuarios/${id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(usuario),
+            });
+            fetchUsuarios();
+        } catch (error) {
+            console.error('Error updating user:', error);
+        }
+    };
+
+    const eliminarUsuario = async (id: number) => {
+        try {
+            await fetch(`/api/usuarios/${id}`, { method: 'DELETE' });
+            fetchUsuarios();
+        } catch (error) {
+            console.error('Error deleting user:', error);
+        }
+    };
+
+    return (
+        <div>
+          {/* Renderizar usuarios y formularios para agregar/actualizar/eliminar usuarios */}
+        </div>
+      );
+};
+
+export default Usuarios;
