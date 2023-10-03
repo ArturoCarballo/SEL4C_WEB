f4ff8f042d1c4266b134d8d6ca66b4b414abf097
ArturoCarballo <a01662245@tec.mx>
2023-10-02 23:14:07 -0600

    Update UserForm.tsx

--- frontend/sel4c-frontend/src/components/UserForm.tsx
+++ frontend/sel4c-frontend/src/components/UserForm.tsx
@@ -45,12 +45,61 @@ export const UserForm: React.FC = () => {
         }
     };
 
-    // Add UI components for form input, and bind handleChange and handleSubmit appropriately
-
     return (
         <div>
             <h2>Añadir Usuario</h2>
-            {/* Your form UI here */}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nombre:
+                    <input type="text" name="nombre" value={user.nombre} onChange={handleChange} required />
+                </label>
+                <br />
+                <label>
+                    Apellido:
+                    <input type="text" name="apellido" value={user.apellido} onChange={handleChange} required />
+                </label>
+                <br />
+                <label>
+                    Email:
+                    <input type="email" name="email" value={user.email} onChange={handleChange} required />
+                </label>
+                <br />
+                <label>
+                    Edad:
+                    <input type="number" name="edad" value={user.edad} onChange={handleChange} required />
+                </label>
+                <br />
+                <label>
+                    Sexo:
+                    <select name="sexo" value={user.sexo} onChange={handleChange} required>
+                        <option value="" disabled>Seleccione</option>
+                        <option value="masculino">Masculino</option>
+                        <option value="femenino">Femenino</option>
+                    </select>
+                </label>
+                <br />
+                <label>
+                    Grado Académico:
+                    <input type="text" name="grado_academico" value={user.grado_academico} onChange={handleChange} required />
+                </label>
+                <br />
+                <label>
+                    Institución:
+                    <input type="text" name="institucion" value={user.institucion} onChange={handleChange} required />
+                </label>
+                <br />
+                <label>
+                    País:
+                    <input type="text" name="pais" value={user.pais} onChange={handleChange} required />
+                </label>
+                <br />
+                <label>
+                    Disciplina:
+                    <input type="text" name="disciplina" value={user.disciplina} onChange={handleChange} required />
+                </label>
+                <br />
+                <button type="submit">Añadir Usuario</button>
+            </form>
         </div>
     );
 }
