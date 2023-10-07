054ce4e1f23b098e98a847a7e257cafdeda15e3c
ArturoCarballo <a01662245@tec.mx>
2023-10-06 20:15:52 -0600

    Enano

--- frontend/sel4c-frontend/src/components/UserProfile.tsx
+++ frontend/sel4c-frontend/src/components/UserProfile.tsx
@@ -1,37 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { User } from '../interface/User';
-import { getUserById } from '../services/User.services';
-import { useParams } from 'react-router-dom';
-import { UserAnswers } from './UserAnswers';
-import './UserProfile.css'
 
-const UserProfile: React.FC = () => {
-    const { id } = useParams();
-    const [user, setUser] = useState<User | null>(null);
+interface UserProfileProps {
+    user: User;
+}
 
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            try {
-                const userDetails = await getUserById(Number(id));
-                setUser(userDetails);
-            } catch (error) {
-                console.error("Error fetching user details:", error);
-            }
-        };
-
-        fetchUserDetails();
-    }, [id]);
-
-    if (!user) return <div>Cargando...</div>;
-
+const UserProfile: React.FC<UserProfileProps> = ({ user }) => {
     return (
         <div>
             <h2>Perfil de {user.nombre} {user.apellido}</h2>
+            {/* Agrega los detalles del usuario aquí */}
             <p>Email: {user.email}</p>
-            <div className="answers-container">
-                <UserAnswers id={user.id || 0} idcuestionario = {1} />
-                <UserAnswers id={user.id || 0} idcuestionario = {2} />
-            </div>
             {/* y así sucesivamente para otros campos del usuario... */}
         </div>
     );
