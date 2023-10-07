7d805ac5b9d4452b2bb3ac5fb91c2197fe8185bb
Carava Jojo <102180250+InakiVA@users.noreply.github.com>
2023-10-07 16:40:50 -0600

    Login Bonito
    login con fondo ajustado, colores de botones y textboxes correctos

--- frontend/sel4c-frontend/src/pages/Login/Login.tsx
+++ frontend/sel4c-frontend/src/pages/Login/Login.tsx
@@ -1,115 +1,141 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { Button, TextField, Typography, Container, Box } from "@mui/material";
 
 async function login(username: string, password: string) {
-    try {
-      const response = await fetch("/api/admin/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ username, password })
-      });
-  
-      const data = await response.json();
-      if (data && data.token) {
-        localStorage.setItem("admin_token", data.token);
-        
-        return data.token;
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error("Error during login:", error);
+  try {
+    const response = await fetch("/api/admin/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+    if (data && data.token) {
+      localStorage.setItem("admin_token", data.token);
+
+      return data.token;
+    } else {
       return null;
     }
+  } catch (error) {
+    console.error("Error during login:", error);
+    return null;
   }
+}
 
 function Login() {
-    const navigate = useNavigate();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const pageStyle = {
-      background: 'linear-gradient(to bottom, #021B79, #ffffff)',
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      boxShadow: '0px 0px 10px rgba(0,0,0,0.1)',
-    };
+  const pageStyle = {
+    background: "linear-gradient(to bottom, #021B79, #ffffff)",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+  };
 
-    const containerStyle = {
-      background: 'rgba(255, 255, 255, 0.8)',
-      padding: '5vh',
-      margin: '5vh auto',
-      borderRadius: '10px',
-      boxShadow: '0px 0px 10px rgba(0,0,0,0.1)',
-    };
+  const titleLabelStyle: React.CSSProperties = {
+    fontWeight: "bold",
+    color: "navy",
+    margin: "25px",
+    fontSize: "40px",
+  };
 
-    const textFieldStyle = {
-        background: 'rgba(255, 255, 255, 0.8)',
-        marginBottom: '10px',
-    };
-    
-  
+  const wordLabelStyle: React.CSSProperties = {
+    fontWeight: "bold",
+    color: "navy",
+    margin: "10px",
+    fontSize: "25px",
+  };
 
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        try {
-            const token = await login(username, password);
-            if (token) {
-                navigate('/users');
-            } else {
-                // Aquí puedes manejar el error y mostrar un mensaje al usuario
-            }
-        } catch (error) {
-            // Muestra un mensaje de error
-        }
-    };
+  const whiteLabelStyle: React.CSSProperties = {
+    fontWeight: "bold",
+    color: "white",
+    marginTop: "25px",
+    marginBottom: "15px",
+    fontSize: "25px",
+  };
 
-    return (
-      
-      <div style={pageStyle}>
+  const containerStyle = {
+    background: "white",
+    //padding: "5vh",
+    margin: "5vh auto",
+    borderRadius: "30px",
+    boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+  };
+
+  const textFieldStyle = {
+    background: "#dfecff",
+    marginBottom: "20px",
+    borderColor: "red", // Set the border color to red
+    borderWidth: "10px", // Optional: You can specify the border width if needed
+    borderRadius: "5px", // Optional: You can specify the border radius if needed
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const token = await login(username, password);
+      if (token) {
+        navigate("/users");
+      } else {
+        // Aquí puedes manejar el error y mostrar un mensaje al usuario
+      }
+    } catch (error) {
+      // Muestra un mensaje de error
+    }
+  };
+
+  return (
+    <div style={pageStyle}>
       <Container component="main" maxWidth="xs" style={containerStyle}>
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            mt: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            mt: 4,
+            mb: 2,
           }}
         >
           <img
-            src="/logo_inicio.png" 
+            src="/logo_inicio.png"
             alt="Imagen de inicio de sesión"
-            style={{ width: '300px', marginBottom: '20px' }}
+            style={{ width: "350px", marginBottom: "20px" }}
           />
-          <Typography component="h1" variant="h3" sx={{ fontSize: '2.5rem' }}>
-              Inicio de sesión
-            </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Typography style={titleLabelStyle}>Inicio de sesión</Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <Typography style={wordLabelStyle}>Nombre de usuario:</Typography>
             <TextField
               variant="outlined"
               margin="normal"
               required
               fullWidth
               id="username"
-              label="Nombre de usuario"
               name="username"
               autoFocus
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               sx={textFieldStyle}
             />
+            <Typography style={wordLabelStyle}>Contraseña:</Typography>
             <TextField
               variant="outlined"
               margin="normal"
               required
               fullWidth
               name="password"
-              label="Contraseña"
               type="password"
               id="password"
               value={password}
@@ -119,9 +145,13 @@ function Login() {
             <Button
               type="submit"
               fullWidth
-              variant="contained"
               color="primary"
-              sx={{ mt: 3, mb: 2, backgroundColor: 'green', '&:hover': { backgroundColor: 'blue' } }}
+              sx={{
+                backgroundColor: "#00cc66",
+                "&:hover": { backgroundColor: "#006633" },
+                borderRadius: "5px",
+              }}
+              style={whiteLabelStyle}
             >
               Iniciar sesión
             </Button>
