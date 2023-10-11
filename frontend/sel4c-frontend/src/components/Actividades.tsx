03d0aefbb39cb3fc0229a12f8b3a430392f5cb05
Carava Jojo <102180250+InakiVA@users.noreply.github.com>
2023-10-10 19:00:44 -0600

    Update Actividades.tsx

--- frontend/sel4c-frontend/src/components/Actividades.tsx
+++ frontend/sel4c-frontend/src/components/Actividades.tsx
@@ -1,20 +1,50 @@
 import React from "react";
+import { Typography, LinearProgress } from "@mui/material";
 
-const titleStyle = {
+const titleStyle: React.CSSProperties = {
   fontWeight: "bold",
   color: "navy",
   fontSize: "40px",
   marginInline: "20px",
 };
 
+const wordLabelStyle: React.CSSProperties = {
+  fontWeight: "bold",
+  color: "navy",
+  fontSize: "25px",
+  marginTop: "20px",
+};
+
+const activityStyle: React.CSSProperties = {
+  flexDirection: "row", // Change to row to display progress bars horizontally
+  display: "flex",
+  alignItems: "center", // Align items vertically in the center
+  margin: "10px",
+  padding: "20px",
+  fontWeight: "bold",
+  color: "black",
+  fontSize: "15px",
+};
+
+const activitiesData: { name: string; progress: number }[] = [
+  { name: "Actividad 1", progress: 100 },
+  { name: "Actividad 2", progress: 80 },
+  { name: "Actividad 3", progress: 60 },
+  { name: "Actividad 4", progress: 40 },
+  { name: "Actividad 5", progress: 20 },
+];
+
 const Actividades: React.FC = () => {
   return (
     <div>
       <h2 style={titleStyle}>Actividades</h2>
-      <p>
-        Aquí irá tu lista de Actividades o cualquier contenido relacionado con
-        las Actividades.
-      </p>
+      <h3 style={wordLabelStyle}>Progreso:</h3>
+      {activitiesData.map((activity, index) => (
+        <div key={index} style={activityStyle}>
+          <Typography>{activity.name}</Typography>
+          <LinearProgress variant="determinate" value={activity.progress} />
+        </div>
+      ))}
     </div>
   );
 };
